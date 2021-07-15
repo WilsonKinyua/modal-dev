a9d2f4234f323bc4e4f0419bb60d5c5e8ff0a303
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-15 23:47:07 +0300

    displaying of modal container on button click

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict"
+
+// select modal class
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const openBtnModal = document.querySelectorAll(".show-modal");
+
+for(let i = 0; i < openBtnModal.length; i++){
+    openBtnModal[i].addEventListener("click", function() {
+        modal.classList.remove("hidden");
+    });
+}
+// console.log(openBtnModal);
