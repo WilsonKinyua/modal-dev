79f2d4eb4a56505213414a728d1debf09f003ed5
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-16 00:22:25 +0300

    closing and opening of modal

--- script.js
+++ script.js
@@ -6,9 +6,28 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const openBtnModal = document.querySelectorAll(".show-modal");
 
-for(let i = 0; i < openBtnModal.length; i++){
-    openBtnModal[i].addEventListener("click", function() {
-        modal.classList.remove("hidden");
-    });
+// function to display the function
+const openModal = function() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 }
-// console.log(openBtnModal);
+
+// function to close the modal and overlay
+const closeModal = function() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+// clicking the button displays the modal
+for(let i = 0; i < openBtnModal.length; i++){
+    openBtnModal[i].addEventListener("click", openModal);
+}
+
+// close the modal window
+btnCloseModal.addEventListener("click", closeModal);
+
+// clicking outside the modal close it
+overlay.addEventListener("click", closeModal);
+
+// listening to keypress 'esc'
+
